004dcff42ab50b0aeed34c8dba56bfd18f4c9539
minhtoan <lebi.singer03@gmail.com>
2025-03-16 21:28:22 +0700

    abc

--- app.py
+++ app.py
@@ -12,8 +12,9 @@ from src import semi_supervised
 # Sidebar navigation
 option = st.sidebar.selectbox("Lựa chọn", ["Titanic Data", "MNIST Data", "Linear Regression",  "SVM Mnist", "Decision Tree Mnist",  "Clustering", "Dim Redution", "Neural Network", "Semi Supervised",  "ML-Flow"])
 
-
-if(option == 'Linear Regression'):
+if(option == 'Titanic Data'):
+    pre_processing.display()
+elif(option == 'Linear Regression'):
     linear_regression.display()
 elif(option == 'MNIST Data'):
     MNIST_understand.display()
